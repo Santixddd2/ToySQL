3a66a6f654f910e3bedc4b26f910385a21287e31
santixddd2 <santixddd222@gmail.com>
2024-01-16 17:41:41 -0500

    Model shapes on config file

--- Classes/Config/config.py
+++ Classes/Config/config.py
@@ -1,15 +1,28 @@
-#This is config document
+#This is config document,currently you can configurate some variables about AI model and image size 
 
 #If the model is in two blocks
 
 convolutional_model="../Model/FirstB"
 clasification_model="../Model/SecondB"
 
-#If the model is in unique block
+#Image shape 
+
+height=28
+weight=28
+
+#Vector size 
+
+vector_size=10
+
+#Comparation Umbral
+
+umbral=0.5
+
+#If the model is in unique block(this function isn't programmed yet XD)
 
 model=""
 
-#Credentials to use the database
+#Credentials to use the database(this function is also not programmed yet XDD)
 
 password=""
 port=""
